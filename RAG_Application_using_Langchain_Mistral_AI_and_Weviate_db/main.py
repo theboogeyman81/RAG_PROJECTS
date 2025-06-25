945e12127cb4c5026ff5fb46597415d6c9fb5c96
theboogeyman81 <theboogeyman8144@gmail.com>
2025-06-25 21:33:31 +0530

    finished up the outline of the code

--- RAG_Application_using_Langchain_Mistral_AI_and_Weviate_db/main.py
+++ RAG_Application_using_Langchain_Mistral_AI_and_Weviate_db/main.py
@@ -2,6 +2,10 @@ from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Weaviate
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.prompts import ChatPromptTemplate
+from langchain import HuggingFaceHub
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.schema.output_parser import StrOutputParser
 
 
 WEAVIATE_URL = ""
@@ -37,6 +41,57 @@ vectorstore = Weaviate.from_documents(
     client=client,
     by_text=False,
 )
+
+
+#print(vectorstore.similarity_search(
+#    query_texts=["What is the main idea of the document?"],
+#    k=3
+#))
+
+
+template = """You are an assistant for question-answering tasks.
+Use the following pieces of retrived context to answer the question.
+If you don't know the answer,just say that you don't know.
+Use ten sentences maxium and keep the answer concise.
+Question: {question}
+Context: {context}
+Answer:
+"""
+
+prompt= ChatPromptTemplate.from_template(template)
+
+#from google.colab import userdata
+#huggingfacehub_api_token = userdata.get('Huggingface_token')
+
+
+model = HuggingFaceHub(
+    huggingfacehub_api_token = huggingfacehub_api_token,
+    repo_id = 'mistralai/Mistral-7B-Instruct-v0.1',
+    model_kwargs={"temperature":1,"max_length":180}
+)
+
+
+output = StrOutputParser()
+
+retriver = vectorstore.as_retriever()
+
+rag_chain= (
+    {"context":retriver,"question":RunnablePassthrough()}
+    |prompt
+    |model
+    |output
+)
+
+
+rag_chain.invoke()
+
+
+
+
+
+
+
+
 
 
 
